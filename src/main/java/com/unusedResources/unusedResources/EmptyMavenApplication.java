e81877680a63c8eb7719669e5e355df0553e79f5
chsyam <syamkumar6845@gmail.com>
2024-09-30 15:48:10 +0530

    added all reguired libraries for empty project

---
+++ src/main/java/com/unusedResources/unusedResources/EmptyMavenApplication.java
@@ -0,0 +1,13 @@
+package com.unusedResources.unusedResources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptyMavenApplication.class, args);
+	}
+
+}
